da3aa00be65c0c5c3961b57ff745fd0c6af902b0
Rome00 <r.khazalia@gmail.com>
2020-05-27 12:42:50 +0400

    edit  icons

---
+++ components/shared/Icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <i class="icon fas" :class="[`fa-${name}`, `icon-size-${size}`]"></i>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'null'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  &-size {
+    &-small {
+      font-size: 15px;
+    }
+    &-normal {
+      font-size: 18px;
+    }
+    &-large {
+      font-size: 21px;
+    }
+  }
+}
+</style>
