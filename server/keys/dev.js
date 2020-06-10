a135ce1f843cb1d62cc67805e7c712c82d191ca3
Rome00 <r.khazalia@gmail.com>
2020-06-10 21:33:10 +0400

    make it better

--- server/keys/dev.js
+++ server/keys/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   DB_URI:
-    'mongodb+srv://Eurus-25:eurus123@cluster0-xlfjw.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    'mongodb+srv://Eurus-25:eurus123@cluster0-xlfjw.mongodb.net/promo_db?retryWrites=true&w=majority',
   SESSION_SECRET: 'aGzrmn36wp'
 }
